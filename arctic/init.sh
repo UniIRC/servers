262de47a44fa932f10c71361dc99dd8f652fa54b
Delan Azabani <delan@azabani.com>
2014-02-11 17:46:14 +0800

    EC2 doesn't provide git by dfeault

--- arctic/init.sh
+++ arctic/init.sh
@@ -6,6 +6,12 @@ log () {
 	printf '\033[35m%s\033[0m\n' "$*"
 }
 
+log Updating package lists
+apt-get -qq update
+
+log Installing git
+apt-get -qq install git
+
 log Cloning GitHub repository with configuration
 git clone https://github.com/UniIRC/servers
 
